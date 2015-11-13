d3195b32fa767dc5bfc828e08e01000ecfdbedb8
Noah Huppert <noahhuppert@gmail.com>
2015-11-13 14:18:14 -0500

    Added existing code

--- HelloSpriteKit/GameScene.swift
+++ HelloSpriteKit/GameScene.swift
@@ -1,45 +1,36 @@
-//
-//  GameScene.swift
-//  HelloSpriteKit
-//
-//  Created by block7 on 11/12/15.
-//  Copyright (c) 2015 block7. All rights reserved.
-//
-
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
+    let player = SKSpriteNode(imageNamed: "player")
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+    override func didMoveToView(view: SKView) {
+        backgroundColor = SKColor.whiteColor()
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        player.position = CGPoint(x: size.height * 0.1, y: size.height * 0.5)
+        
+        addChild(player)
+        
+        for var i = 0; i < 10; i++ {
+            spawnMonster()
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    func spawnMonster() {
+        let monster = SKSpriteNode(imageNamed: "monster")
+        
+        monster.position = CGPoint(
+            x: size.width + monster.size.width,
+            y: CGFloat(arc4random_uniform(UInt32(Float(size.height))))
+        )
+        
+        addChild(monster)
+        
+        let actionMove = SKAction.moveTo(
+            CGPoint(x: -monster.size.width, y: monster.position.y),
+            duration: NSTimeInterval())
+        
+        let actionMoveDone = SKAction.removeFromParent()
+        
+        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
     }
 }
