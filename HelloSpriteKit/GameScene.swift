fa59c68dfd2d5dcfe069462f70c1729624098c32
Noah Huppert <Noah-Huppert@Noahs-MBP.home>
2015-11-14 23:40:00 -0500

    Made ghosts move, started ninja star throwing

--- HelloSpriteKit/GameScene.swift
+++ HelloSpriteKit/GameScene.swift
@@ -10,8 +10,32 @@ class GameScene: SKScene {
         
         addChild(player)
         
-        for var i = 0; i < 10; i++ {
-            spawnMonster()
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(spawnMonster),
+                SKAction.waitForDuration(1.0)
+            ])
+        ))
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            let touchLocation = touch.locationInNode(self)
+            let touchScaler = (size.height - touchLocation.y) / touchLocation.y// TODO: Figure out how to make destination of ninja star be offscreen with triangles of same ratios
+            
+            let ninjaStar = SKSpriteNode(imageNamed: "projectile")
+            
+            ninjaStar.position = player.position
+            
+            addChild(ninjaStar)
+            
+            let moveAction = SKAction.moveTo(CGPoint(
+                                                x: touchLocation.x * touchScaler,
+                                                y: touchLocation.y * touchScaler),
+                duration: 1.5)
+            let moveDoneAction = SKAction.removeFromParent()
+            
+            ninjaStar.runAction(SKAction.sequence([moveAction, moveDoneAction]))
         }
     }
     
@@ -20,17 +44,21 @@ class GameScene: SKScene {
         
         monster.position = CGPoint(
             x: size.width + monster.size.width,
-            y: CGFloat(arc4random_uniform(UInt32(Float(size.height))))
+            y: CGFloat(Random(Int(monster.size.height / CGFloat(2)), max: Int(size.height - monster.size.height / 2)))
         )
         
         addChild(monster)
         
         let actionMove = SKAction.moveTo(
-            CGPoint(x: -monster.size.width, y: monster.position.y),
-            duration: NSTimeInterval())
+            CGPoint(x: -monster.size.width / 2, y: monster.position.y),
+            duration: NSTimeInterval(Random(2, max: 4)))
         
         let actionMoveDone = SKAction.removeFromParent()
         
         monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+    }
+    
+    func Random(min: Int, max: Int) -> Int {
+        return Int(arc4random_uniform(UInt32((max - min) + 1))) + min
     }
 }
